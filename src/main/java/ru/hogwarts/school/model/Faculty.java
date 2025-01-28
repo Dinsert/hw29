7a2d6aa458e5367e04548e4a995d3ceb2c275d9e
Наталья <korolkov.01@bk.ru>
2025-01-28 14:42:15 +0300

    edit homework29.3

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,10 +20,12 @@ public class Faculty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "color")
     private String color;
-    @JsonIgnore
     @OneToMany(mappedBy = "faculty")
+    @JsonManagedReference
     private List<Student> students;
 
     public Faculty(long id, String name, String color) {
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,9 +1,13 @@
 package ru.hogwarts.school.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +22,12 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "age")
     private int age;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "faculty_id")
+    @JsonBackReference
     private Faculty faculty;
 }
