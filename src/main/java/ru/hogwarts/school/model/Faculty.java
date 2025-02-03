dc89a5cf0398a62d3bc9ce6016f0b486d0b6d7ac
Наталья <korolkov.01@bk.ru>
2025-02-03 13:42:59 +0300

    for resolve conflict merge with branch hw29.3 try 2

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,14 +1,9 @@
 package ru.hogwarts.school.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
