417d03c48c96b793a601889178fa3602f2487472
Наталья <korolkov.01@bk.ru>
2025-02-03 14:09:13 +0300

    for resolve conflict merge with branch hw29.3 try 6

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -2,4 +2,7 @@ package ru.hogwarts.school.model;
 
 public class Faculty {
 
+    public Faculty(long id, String name, String color) {
+
+    }
 }
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -2,4 +2,7 @@ package ru.hogwarts.school.model;
 
 public class Student {
 
+    public Student(long id, String harry, int i, Faculty faculty) {
+
+    }
 }
