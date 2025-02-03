91492d67d6ffb06172eaf3d2de3494b0449c3430
Наталья <korolkov.01@bk.ru>
2025-02-03 13:51:47 +0300

    for resolve conflict merge with branch hw29.3 try 3

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -4,18 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @Entity(name = "faculties")
 public class Faculty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
     public Faculty(long id, String name, String color) {
         this.id = id;
         this.name = name;
